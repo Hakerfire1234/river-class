e1fe28f06bcd93aef37deb47203d3886850d36c4
Hakerfire1234 <136265588+Hakerfire1234@users.noreply.github.com>
2023-11-19 12:31:25 -0500

    Update myriver.py

--- myriver.py
+++ myriver.py
@@ -28,29 +28,3 @@ class myriver:
         return self.river
 
 
-# Example of using myriver
-def example_river_usage():
-    river_manager = myriver()
-
-    print("Initial River State:", river_manager.current_state())
-
-    # Add elements to the river
-    for _ in range(10):
-        river_manager.add_stream()
-        print("After Adding Element:", river_manager.current_state())
-
-    # Change the value at index 1
-    river_manager.flow(1, 5)
-    print("After Changing Element at Index 1:", river_manager.current_state())
-
-    # Block elements from index 4
-    river_manager.block_stream(4)
-    print("After Blocking Elements from Index 4:",
-          river_manager.current_state())
-
-    # Check if the river is empty
-    print("Is River Empty?", river_manager.is_empty())
-
-
-# Run the example
-example_river_usage()
